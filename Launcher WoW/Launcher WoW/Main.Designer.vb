fb62fb91b2bd68ea32f12492364a3690c594ba4b
Sl4yZ <rxzeditor@gmail.com>
2013-10-20 14:36:21 +0200

    Codage de tous les boutons terminé! Forum, Site, Realmlist, Vider le cache, joueur (a condition que le serveur soit ON)

--- Launcher WoW/Launcher WoW/Main.Designer.vb
+++ Launcher WoW/Launcher WoW/Main.Designer.vb
@@ -25,14 +25,22 @@ Partial Class Main
         Me.btnPlay = New System.Windows.Forms.Button()
         Me.lblServerStatut = New System.Windows.Forms.Label()
         Me.lblStatut = New System.Windows.Forms.Label()
+        Me.vidCache = New System.Windows.Forms.Button()
+        Me.defRealm = New System.Windows.Forms.Button()
+        Me.btnForum = New System.Windows.Forms.Button()
+        Me.btnSite = New System.Windows.Forms.Button()
+        Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
+        Me.WebBrowser1 = New System.Windows.Forms.WebBrowser()
+        Me.CopyRight = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'btnPlay
         '
         Me.btnPlay.Enabled = False
-        Me.btnPlay.Location = New System.Drawing.Point(467, 331)
+        Me.btnPlay.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnPlay.Location = New System.Drawing.Point(395, 291)
         Me.btnPlay.Name = "btnPlay"
-        Me.btnPlay.Size = New System.Drawing.Size(190, 63)
+        Me.btnPlay.Size = New System.Drawing.Size(190, 105)
         Me.btnPlay.TabIndex = 0
         Me.btnPlay.Text = "Jouer"
         Me.btnPlay.UseVisualStyleBackColor = True
@@ -40,7 +48,7 @@ Partial Class Main
         'lblServerStatut
         '
         Me.lblServerStatut.AutoSize = True
-        Me.lblServerStatut.Location = New System.Drawing.Point(474, 315)
+        Me.lblServerStatut.Location = New System.Drawing.Point(465, 0)
         Me.lblServerStatut.Name = "lblServerStatut"
         Me.lblServerStatut.Size = New System.Drawing.Size(93, 13)
         Me.lblServerStatut.TabIndex = 1
@@ -50,23 +58,102 @@ Partial Class Main
         '
         Me.lblStatut.AutoSize = True
         Me.lblStatut.ForeColor = System.Drawing.Color.Red
-        Me.lblStatut.Location = New System.Drawing.Point(575, 315)
+        Me.lblStatut.Location = New System.Drawing.Point(564, 0)
         Me.lblStatut.Name = "lblStatut"
         Me.lblStatut.Size = New System.Drawing.Size(27, 13)
         Me.lblStatut.TabIndex = 2
         Me.lblStatut.Text = "OFF"
         '
+        'vidCache
+        '
+        Me.vidCache.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.vidCache.Location = New System.Drawing.Point(199, 344)
+        Me.vidCache.Name = "vidCache"
+        Me.vidCache.Size = New System.Drawing.Size(190, 52)
+        Me.vidCache.TabIndex = 3
+        Me.vidCache.Text = "Vider le Cache"
+        Me.vidCache.UseVisualStyleBackColor = True
+        '
+        'defRealm
+        '
+        Me.defRealm.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.defRealm.Location = New System.Drawing.Point(199, 291)
+        Me.defRealm.Name = "defRealm"
+        Me.defRealm.Size = New System.Drawing.Size(190, 52)
+        Me.defRealm.TabIndex = 4
+        Me.defRealm.Text = "Définir le Realmlist"
+        Me.defRealm.UseVisualStyleBackColor = True
+        '
+        'btnForum
+        '
+        Me.btnForum.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnForum.Location = New System.Drawing.Point(3, 291)
+        Me.btnForum.Name = "btnForum"
+        Me.btnForum.Size = New System.Drawing.Size(190, 52)
+        Me.btnForum.TabIndex = 6
+        Me.btnForum.Text = "Forum"
+        Me.btnForum.UseVisualStyleBackColor = True
+        '
+        'btnSite
+        '
+        Me.btnSite.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSite.Location = New System.Drawing.Point(3, 344)
+        Me.btnSite.Name = "btnSite"
+        Me.btnSite.Size = New System.Drawing.Size(190, 52)
+        Me.btnSite.TabIndex = 5
+        Me.btnSite.Text = "Site Web"
+        Me.btnSite.UseVisualStyleBackColor = True
+        '
+        'LinkLabel1
+        '
+        Me.LinkLabel1.AutoSize = True
+        Me.LinkLabel1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LinkLabel1.Location = New System.Drawing.Point(0, 0)
+        Me.LinkLabel1.Name = "LinkLabel1"
+        Me.LinkLabel1.Size = New System.Drawing.Size(191, 13)
+        Me.LinkLabel1.TabIndex = 7
+        Me.LinkLabel1.TabStop = True
+        Me.LinkLabel1.Text = "Pas encore de compte? Inscrivez-vous"
+        '
+        'WebBrowser1
+        '
+        Me.WebBrowser1.Location = New System.Drawing.Point(3, 16)
+        Me.WebBrowser1.MinimumSize = New System.Drawing.Size(20, 20)
+        Me.WebBrowser1.Name = "WebBrowser1"
+        Me.WebBrowser1.Size = New System.Drawing.Size(582, 269)
+        Me.WebBrowser1.TabIndex = 8
+        Me.WebBrowser1.Url = New System.Uri("http://google.fr", System.UriKind.Absolute)
+        '
+        'CopyRight
+        '
+        Me.CopyRight.AutoSize = True
+        Me.CopyRight.BackColor = System.Drawing.Color.Transparent
+        Me.CopyRight.Font = New System.Drawing.Font("Segoe Print", 6.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.CopyRight.Location = New System.Drawing.Point(272, -3)
+        Me.CopyRight.Name = "CopyRight"
+        Me.CopyRight.Size = New System.Drawing.Size(47, 20)
+        Me.CopyRight.TabIndex = 9
+        Me.CopyRight.Text = "By Sl4yZ"
+        Me.CopyRight.UseCompatibleTextRendering = True
+        '
         'Main
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(669, 406)
+        Me.ClientSize = New System.Drawing.Size(591, 398)
+        Me.Controls.Add(Me.CopyRight)
+        Me.Controls.Add(Me.WebBrowser1)
+        Me.Controls.Add(Me.LinkLabel1)
+        Me.Controls.Add(Me.btnForum)
+        Me.Controls.Add(Me.btnSite)
+        Me.Controls.Add(Me.defRealm)
+        Me.Controls.Add(Me.vidCache)
         Me.Controls.Add(Me.lblStatut)
         Me.Controls.Add(Me.lblServerStatut)
         Me.Controls.Add(Me.btnPlay)
         Me.Name = "Main"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
-        Me.Text = "Form1"
+        Me.Text = "Launcher VOTRE SERVEUR"
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -74,5 +161,12 @@ Partial Class Main
     Friend WithEvents btnPlay As System.Windows.Forms.Button
     Friend WithEvents lblServerStatut As System.Windows.Forms.Label
     Friend WithEvents lblStatut As System.Windows.Forms.Label
+    Friend WithEvents vidCache As System.Windows.Forms.Button
+    Friend WithEvents defRealm As System.Windows.Forms.Button
+    Friend WithEvents btnForum As System.Windows.Forms.Button
+    Friend WithEvents btnSite As System.Windows.Forms.Button
+    Friend WithEvents LinkLabel1 As System.Windows.Forms.LinkLabel
+    Friend WithEvents WebBrowser1 As System.Windows.Forms.WebBrowser
+    Friend WithEvents CopyRight As System.Windows.Forms.Label
 
 End Class
--- Launcher WoW/Launcher WoW/Main.vb
+++ Launcher WoW/Launcher WoW/Main.vb
@@ -33,12 +33,56 @@ Public Class Main
         End If
     End Sub
 
-    Private Sub Main_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
-    End Sub
-
     Private Sub Main_Shown(sender As Object, e As EventArgs) Handles Me.Shown
         'Execution de la fonction logTest lorsque le logiciel est chargé
         logTest()
+    End Sub
+           
+    Private Sub defRealm_Click(sender As Object, e As EventArgs) Handles defRealm.Click
+        'Si le joueur a le client en Français (d'où le frFR)
+        Try
+            Dim FILE_NAME As String = "\Data\frFR\realmlist.wtf"
+            'Condition si le fichier existe
+            If System.IO.File.Exists(FILE_NAME) = True Then
+                Dim objWriter As New System.IO.StreamWriter(FILE_NAME)
+                'Ecriture du realmlist dans le fichier realmlist.wtf
+                objWriter.Write("set realmlist L'IP DE TON SERVEUR")
+                objWriter.Close()
+                MsgBox("Realmlist changé avec succès!", MsgBoxStyle.Information)
+            Else
+
+                MsgBox("Vous n'avez pas de fichier realmlist.wtf, veuillez en créer un dans le dossier Data\frFR\")
+
+            End If
+        Catch ex As Exception 'Si erreur, alors il test si le joueur a un client en Anglais (enUS)
+            MsgBox("Vous ne devez pas avoir un client en Français. Test d'édition du realmlist pour un client en Anglais ...", MsgBoxStyle.Exclamation)
+            Dim FILE_NAME As String = "\Data\enUS\realmlist.wtf"
+            'Condition si le fichier existe
+            If System.IO.File.Exists(FILE_NAME) = True Then
+                Dim objWriter As New System.IO.StreamWriter(FILE_NAME)
+                'Ecriture du realmlist dans le fichier realmlist.wtf
+                objWriter.Write("set realmlist L'IP DE TON SERVEUR")
+                objWriter.Close()
+                MsgBox("Realmlist changé avec succès!", MsgBoxStyle.Information)
+            Else
+
+                MsgBox("Vous n'avez pas de fichier realmlist.wtf, veuillez en créer un dans le dossier Data\enUS\")
+            End If
+        End Try
+        ' Vous pouvez ajouter des conditions comme ça à l'infini (si le joueur a un client en italien ou espagnol ou ...)
+    End Sub
+
+    Private Sub vidCache_Click(sender As Object, e As EventArgs) Handles vidCache.Click
+        Dim dossierCache As String = "\Cache"
+        'Suppression de tout le dossier Cache
+        System.IO.Directory.Delete(dossierCache, True)
+    End Sub
+
+    Private Sub btnForum_Click(sender As Object, e As EventArgs) Handles btnForum.Click
+        Process.Start("HTTP://URL DE VOTRE FORUM/")
+    End Sub
+
+    Private Sub btnSite_Click(sender As Object, e As EventArgs) Handles btnSite.Click
+        Process.Start("HTTP://URL DE VOTRE SITE/")
     End Sub
 End Class
